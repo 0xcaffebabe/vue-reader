bf326c07fab3a8ee561241a396d6133650454063
MY <715711877@qq.com>
2020-05-11 09:36:57 +0800

    项目搭建及初始化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <span class="icon"></span>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +9,19 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  console.log(fontSize)
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/EBook.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="read-wrapper">
+      <div id="read"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const url = '/book.epub'
+export default {
+  data () {
+    return {
+      book: {}
+    }
+  },
+  methods: {
+    showEpub () {
+      // 生成Book
+      this.book = new Epub(url)
+      console.log(this.book)
+      // 生成 Rendition
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 渲染电子书
+      this.rendition.display()
+    }
+  },
+  created() {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/global';
+</style>
