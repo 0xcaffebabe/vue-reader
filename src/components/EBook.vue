c8f53da0447495729c38bed898b2d6044351ac20
MY <715711877@qq.com>
2020-05-12 11:39:24 +0800

    完成前后翻页

--- src/components/EBook.vue
+++ src/components/EBook.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <div class="ebook">
     <div class="read-wrapper">
       <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="previous"></div>
+        <div class="center"></div>
+        <div class="right" @click="next"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,9 +32,19 @@ export default {
       })
       // 渲染电子书
       this.rendition.display()
+    },
+    previous () {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    next () {
+      if (this.rendition) {
+        this.rendition.next()
+      }
     }
   },
-  created() {
+  created () {
     this.showEpub()
   }
 }
@@ -37,4 +52,30 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/styles/global';
+.ebook {
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: flex;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      .left {
+        flex: 0 0 px2rem(100);
+        // background: pink;
+      }
+      .center {
+        flex: 1;
+        // background: skyblue;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+        // background: deepskyblue;
+      }
+    }
+  }
+}
 </style>
